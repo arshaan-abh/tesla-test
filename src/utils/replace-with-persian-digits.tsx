9d47745f8068d488b901813945e3b9a873bce061
arshaan-abh <arshaan.git@gmail.com>
2024-04-24 15:36:48 +0330

    fixed a utility function

--- src/utils/replace-with-persian-digits.tsx
+++ src/utils/replace-with-persian-digits.tsx
@@ -1,7 +1,8 @@
 function replaceWithPersianDigits(string: string) {
   const persianDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
   let res = string;
-  for (let i = 0; i < 9; i++) res = res.replace(i.toString(), persianDigits[i]);
+  for (let i = 0; i < 9; i++)
+    res = res.replaceAll(i.toString(), persianDigits[i]);
   return res;
 }
 
