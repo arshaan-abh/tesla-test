f1f30b0cc1a6bf019a84f1a4bc3ac740cdfa65b1
arshaan-abh <arshaan.git@gmail.com>
2024-04-26 18:00:44 +0330

    separate products data from home page

---
+++ src/constants/products.tsx
@@ -0,0 +1,83 @@
+import Product from "@/components/home/product";
+import product from "/public/product.jpg";
+
+const products: Product[] = [
+  {
+    id: 0,
+    name: "کراپ‌تاپ آستین بلند زنانه ماییلدا مدل 4423-437 رنگ مشکی ...",
+    images: [product, product, product, product],
+    colors: [
+      { color: "#383837" },
+      { color: "#865222" },
+      { color: "#C5FBE4" },
+      { color: "#D67777", notification: true },
+      { color: "#323F35", notification: true },
+      { color: "#323F35" },
+      { color: "#323F35" },
+    ],
+    freeTransport: true,
+    stock: 11,
+    score: 4.8,
+    price: 80000000,
+    priceWithoutDiscount: 120000000,
+    discountPercent: 20,
+  },
+  {
+    id: 1,
+    name: "کراپ‌تاپ آستین بلند زنانه",
+    images: [product, product, product, product],
+    colors: [
+      { color: "#383837", image: product },
+      { color: "#865222", image: product },
+      { color: "#C5FBE4", image: product },
+      { color: "#D67777", image: product },
+      { color: "#323F35", image: product },
+      { color: "#323F35", image: product },
+      { color: "#323F35", image: product },
+    ],
+    freeTransport: true,
+    stock: 5,
+    score: 4.8,
+    price: 120000000,
+  },
+  {
+    id: 2,
+    name: "کراپ‌تاپ آستین بلند زنانه",
+    images: [product, product, product, product],
+    colors: [
+      { color: "#383837" },
+      { color: "#865222" },
+      { color: "#C5FBE4" },
+      { color: "#D67777" },
+      { color: "#323F35" },
+      { color: "#323F35" },
+      { color: "#323F35" },
+    ],
+    stock: 0,
+    score: 4.8,
+    price: 120000000,
+  },
+  {
+    id: 3,
+    name: "کراپ‌تاپ آستین بلند زنانه ماییلدا مدل 4423-437 رنگ مشکی ...",
+    images: [product, product, product, product],
+    colors: [
+      { color: "#383837" },
+      { color: "#865222" },
+      { color: "#C5FBE4" },
+      { color: "#D67777" },
+      { color: "#323F35" },
+      { color: "#323F35" },
+      { color: "#323F35" },
+    ],
+    freeTransport: true,
+    stock: 11,
+    score: 4.8,
+    price: 80000000,
+    priceWithoutDiscount: 120000000,
+    discountPercent: 20,
+    discountDeadline: "02:24:36",
+  },
+];
+
+export default products;
